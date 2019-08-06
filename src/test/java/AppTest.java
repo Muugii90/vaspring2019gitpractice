2eb7bffdcf175e287f943245ddc8a2edbe90c32a
Munkhnasan <moogii.mogi@gmail.com>
2019-08-06 17:45:49 -0400

    add add method

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,5 +2,13 @@ public class AppTest {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
+
+
+
     }
 }
