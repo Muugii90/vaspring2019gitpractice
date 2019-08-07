94b131cc341910c743dbffdacd7ba663329f7fce
Muugii90 <50155318+Muugii90@users.noreply.github.com>
2019-08-06 20:35:39 -0400

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,7 +1,8 @@
 public class AppTest {
     public static void main(String[] args) {
         System.out.println("Yo");
-
+          System.out.println("Yo");
+          System.out.println("Yo");
 
 
 
