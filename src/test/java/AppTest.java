241150f5ea91b925ceec62cfd5b0cf231fd7f59d
Muugii90 <50155318+Muugii90@users.noreply.github.com>
2019-08-06 20:19:52 -0400

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,11 +1,7 @@
 public class AppTest {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Good-Bye");
-        System.out.println("Good morning");
-        System.out.println("Hello");
+        System.out.println("Yo");
+
 
 
 
