a8d79fb6ff224960968e904feae063918373c5ac
Muugii90 <50155318+Muugii90@users.noreply.github.com>
2019-08-06 21:26:54 -0400

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,7 +1,9 @@
 public class LoginTest {
 
     public static void main(String[] args) {
-        System.out.println("Line 1");
-        System.out.println("Line 2");
+        System.out.println("Line 0");
+        System.out.println("Line 0");
+        System.out.println("Line 0");
+        System.out.println("Line 0");
     }
 }
