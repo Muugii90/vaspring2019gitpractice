be79f3536e186abf12cac860f4999b477307eaca
Munkhnasan <moogii.mogi@gmail.com>
2019-08-06 21:38:54 -0400

    changed . again

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,13 +2,8 @@ public class LoginTest {
 
     public static void main(String[] args) {
 
-        System.out.println("Line 1");
-        System.out.println("Line 2");
-        System.out.println("Line 3");
+        System.out.println("Line 11");
 
-        System.out.println("Line 0");
-        System.out.println("Line 0");
-        System.out.println("Line 0");
 
     }
 }
