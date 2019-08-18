ea65b313894342841bb73990535e2023816ba097
Muugii90 <50155318+Muugii90@users.noreply.github.com>
2019-08-18 10:00:53 -0400

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -12,7 +12,10 @@ public class LoginTest {
         System.out.println("Line 411");
         System.out.println("This line coming from develop branch");
 
-
+        System.out.println("Line 211");
+        System.out.println("Line 211");
+        System.out.println("Line 411");
+        System.out.println("This line coming from develop branch");
 
     }
 }
