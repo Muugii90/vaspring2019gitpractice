b6bf82dfc2febd4e3f06de4af58858f438e65a99
Muugii90 <50155318+Muugii90@users.noreply.github.com>
2019-08-18 15:01:02 -0400

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,14 +1,10 @@
 public class LoginTest {
 
     public static void main(String[] args) {
-
-
-       System.out.println("Line 11");
-        System.out.println("Line wddcdjcbd411");
-        System.out.println("Linecf nj bj 211");
-        System.out.println("Line cbcbd bd211");
-        System.out.println("Line 411");
-
-
+        System.out.println("Line 12");
+        System.out.println("Line moogi");
+        System.out.println("150k");
+        System.out.println("resume done");
+        System.out.println("wwwww");
     }
 }
